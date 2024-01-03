2ada7f7c0b5ed4a2f1cb3cbfba8487d3488871b0
Potapenko <nylestroke@gmail.com>
2024-01-03 11:16:51 +0100

    Initial commit

---
+++ OOP1/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Person
+    {
+        public string FirstName { get; private set; } = string.Empty;
+
+        public string LastName { get; private set; } = string.Empty;
+
+        public DateTime Birthday { get; private set; }
+
+        public string Sex { get; private set; } = "Not set";
+
+        public Person() { }
+
+        public Person(string firstName, string lastName, DateTime birthday, string sex)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Birthday = birthday;
+            this.Sex = sex;
+        }
+
+        public int GetAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - this.Birthday.Year;
+            if (this.Birthday.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP1;
+
+Random random = new Random();
+DateTime birthday = new DateTime(2007, 8, 24, 13, 15, 00);
+var person = new Person("Vladyslav", "Potapenko", birthday, "Male");
+
+Console.WriteLine($" {person.FirstName} {person.LastName} birthday: {person.Birthday} \n " +
+    $"Age: {person.GetAge()} \n Sex: {person.Sex}");
